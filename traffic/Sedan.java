4a573deab787d6d2d86601cee22ce158f30a7a57
McClane Howland <mcclane.howland@gmail.com>
2016-03-10 20:24:14 -0800

    new sedan

--- traffic/Sedan.java
+++ traffic/Sedan.java
@@ -14,5 +14,16 @@ public class Sedan extends MediumCar {
         g.setColor(Color.black);
         g.drawRect(x+20,y,15,20);
         g.drawRect(x+35,y,15,20);
+        int[] xArray = new int[4];
+        int[] yArray = new int[4];
+        xArray[0] = x+10;
+        yArray[0] = y;
+        xArray[1] = x+25;
+        yArray[1] = y-10;
+        xArray[2] = x+45;
+        yArray[2] = y-10;
+        xArray[3] = x+55;
+        yArray[3] = y;
+        g.fillPolygon(xArray,yArray,4);
     }
 }
